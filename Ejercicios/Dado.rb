187990ac7b016f169c917b3b5b2d41e23230cc07
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-05-14 21:53:26 -0400

    Se añade nuevo archivo Dado.rb
    Se añadio un nuevo archivo Dado.rb

--- Ejercicios/Dado.rb
+++ Ejercicios/Dado.rb
@@ -1,1 +1,16 @@
- 
+class Principal 
+	def main
+		d = Dado.new
+
+		puts d.lanzar
+	end
+end 
+
+class Dado
+	def lanzar
+		return (rand * 6 + 1).to_i
+	end
+end
+
+principal = Principal.new
+principal.main
